2ac4a26ad9f39fd074d8a99add4c89f7dd053429
GitVishal <vishalmishra6604@gmail.com>
2021-12-26 19:03:46 +0530

    cors error solve

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/config");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("app is working...");
+app.use(express.json());
+app.use(cors());
+const User = require("./db/User");
+app.post("/Register", async (req, res) => {
+  let user = new User(req.body);
+  let result  = await user.save();
+  res.send(req.body);
 });
 
 app.listen(5000);
